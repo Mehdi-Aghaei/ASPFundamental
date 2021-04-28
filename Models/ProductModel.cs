c2fc8c6c7a08e18a439cf8766a6f1c558a653d84
Cacti79 <mehdi1020@yahoo.com>
2021-04-28 12:22:33 +0200

    add Product model

---
+++ Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Refrence.Models
+{
+    public class ProductModel
+    {
+        // we use required attributes for data control and being more spesific
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        [DisplayName("Enter full name")]
+        public string Name { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+    }
+}
