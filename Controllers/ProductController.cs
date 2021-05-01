410b54774d86137f73ad2de3a4f566ff659828b4
Cacti79 <mehdi1020@yahoo.com>
2021-05-01 16:49:34 +0200

    add Ajax and partial view option also commented

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 /*
  * we will use all methods from service class here and we can create view of that or form or list
  * we can create method that get model or int(id) to work with and then we can say in which template we wanto to see it
- * also the first thing we have to use is to initialize or service first and then use methods
+ * also the first thing we have to use is to initialize or service(DAO) as global and then constructor first and then use methods
  * and the viw method can get template name in String and a method also 
  * and also we have to think how we can use this method 
  * we want to send it somewhere or its just query data from data base we want to update or delete and ... and usually we use forms to post data in in anothr method by call it 
@@ -18,43 +18,43 @@ namespace Refrence.Controllers
 {
     public class ProductController : Controller
     {
+        ProductsDAO repository;
+        public ProductController()
+        {
+            repository = new();
+        }
         public IActionResult Index()
         {
-            ProductsDAO products = new();
-            return View(products.GetAllProducts());
+            return View(repository.GetAllProducts());
         }
         public IActionResult SearchResults(string searchTerm)
         {
-            ProductsDAO products = new();
-            List<ProductModel> productList = products.SearchProducts(searchTerm);
+            List<ProductModel> productList = repository.SearchProducts(searchTerm);
             return View("Index", productList);
         }
         public IActionResult ShowDetails(int id)
         {
-            ProductsDAO products = new();
-            ProductModel foundProduct = products.GetProductByID(id);
+            ProductModel foundProduct = repository.GetProductByID(id);
             return View(foundProduct);
         }
         public IActionResult Edit(int id)
         {
-            ProductsDAO products = new();
-            ProductModel foundProduct = products.GetProductByID(id);
+
+            ProductModel foundProduct = repository.GetProductByID(id);
             return View("ShowEdit", foundProduct);
         }
         // what we accept is goes to that argument that function will take
         public IActionResult ProcessEdit(ProductModel product)
         {
-            ProductsDAO products = new();
-            products.Update(product);
-            return View("Index", products.GetAllProducts());
+            repository.Update(product);
+            return View("Index", repository.GetAllProducts());
 
         }
         public IActionResult Delete(int id)
         {
-            ProductsDAO products = new();
-            ProductModel product = products.GetProductByID(id);
-            products.Delete(product);
-            return View("Index", products.GetAllProducts());
+            ProductModel product = repository.GetProductByID(id);
+            repository.Delete(product);
+            return View("Index", repository.GetAllProducts());
 
         }
         public IActionResult Create()
@@ -63,9 +63,8 @@ namespace Refrence.Controllers
         }
         public IActionResult ProcessCreate(ProductModel product)
         {
-            ProductsDAO products = new();
-            products.Insert(product);
-            return View("Index", products.GetAllProducts());
+            repository.Insert(product);
+            return View("Index", repository.GetAllProducts());
         }
 
 
@@ -73,5 +72,17 @@ namespace Refrence.Controllers
         {
             return View();
         }
+        public IActionResult ShowDetailsJson(int id)
+        {
+            ProductModel foundProduct = repository.GetProductByID(id);
+            return Json(foundProduct);
+        }
+        public IActionResult ProcessEditReturnPartial(ProductModel product)
+        {
+            repository.Update(product);
+            return PartialView("_productCard", product);
+
+        }
+
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,57 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*
+ * to focous read code 2 times and check the console in browswe and see what we have there
+ */
 
-// Write your JavaScript code.
+$(function () {
+    console.log("its fine")
+    $(document).on("click", ".edit-product-button", function () {
+        console.log("you just click number" + $(this).val());
+        // store the product id number
+        var productID = $(this).val();
+        $.ajax({
+            type: "json",
+            data: {
+                "id": productID
+            },
+            url: "/Product/ShowDetailsJson",
+            success: function (data) {
+                console.log(data)
+                // fill in the input fields in the modal
+                $("#modal-input-id").val(data.id);
+                $("#modal-input-name").val(data.name);
+                $("#modal-input-price").val(data.price);
+                $("#modal-input-info").val(data.info);
+
+
+
+
+            }
+
+        });
+    });
+    $("#save-button").click(function () {
+        // get values from the input fields and create a json object to submit to the controller
+        var Product = {
+            "Id": $("#modal-input-id").val(),
+            "Name": $("#modal-input-name").val(),
+            "Price": $("#modal-input-price").val(),
+            "Info": $("#modal-input-info").val()
+        };
+        console.log("Saved");
+        console.log(Product);
+        // save the updated product record in the database using controller.
+        $.ajax({
+            type: "json",
+            data: Product,
+            url: "/Product/ProcessEditReturnPartial",
+            success: function (data) {
+                console.log(data);
+                // it will hide and apear after 1 sec
+                $("#card-number-" + Product.Id).html(data).hide().fadeIn(1000);
+            }
+
+
+        });
+
+    });
+});
